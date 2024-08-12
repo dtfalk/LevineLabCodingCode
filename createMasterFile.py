193d11793ec23f0d6694d8c68deb4c9292a7b94e
David Falk <dtfalk@uchicago.edu>
2024-08-11 22:25:56 -0400

    style fix

--- createMasterFile.py
+++ createMasterFile.py
@@ -97,7 +97,7 @@ def getResults(subjectFile, transcriptsFolderPath):
             # skip this label if it is a blank line,
             # if it is the last line (the files sent to me had a list of 0's as the very last line),
             # or if there is a header (the files sent to me had 3 lines of headers so I needed to skip those lines)
-            if len(line) == 0:# or lineIndex < 3 or lineIndex == len(lines) - 1:
+            if len(line) == 0: # or lineIndex < 3 or lineIndex == len(lines) - 1:
                 continue
             
             # extract the label from the line, make lowercase, remove all spaces, and split by dashes
